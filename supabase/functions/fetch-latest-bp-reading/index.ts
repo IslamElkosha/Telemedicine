8a66c81dab510b30490a6686a50affd05b65da38
IslamElkosha <elkoshaislam@gmail.com>
2025-11-15 20:22:57 +0200

    Updated index.ts

--- supabase/functions/fetch-latest-bp-reading/index.ts
+++ supabase/functions/fetch-latest-bp-reading/index.ts
@@ -12,7 +12,7 @@ interface BPReading {
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, cache-control',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, cache-control, pragma',
   'Cache-Control': 'no-cache, no-store, must-revalidate',
   'Pragma': 'no-cache',
   'Expires': '0',
